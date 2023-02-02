5587ac7837c4d5967f296c66fd29239941c7cb0f
k11m2ok <k11m2ok@gmail.com>
2023-02-02 11:48:51 +0900

    新規にプロジェクトを作成

---
+++ src/main/java/com/example/GitSampleMatsuokaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleMatsuokaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleMatsuokaApplication.class, args);
+	}
+
+}
